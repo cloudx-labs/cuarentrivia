0d385e3a58a78eb314a2739030d62031b78aa989
apolo <agustin.polo@cloudx.com>
2020-09-11 12:40:35 -0300

    fix typo

--- apps/www/src/app/login/index.tsx
+++ apps/www/src/app/login/index.tsx
@@ -41,7 +41,7 @@ const Login = () => {
       <main className="login">
         <section className="login-content">
           <img src={logo} alt="Logo" className="login-content-logo" />
-          <h1 className="login-content-title">Sing In</h1>
+          <h1 className="login-content-title">Sign In</h1>
           <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseAuth} />
         </section>
       </main>
