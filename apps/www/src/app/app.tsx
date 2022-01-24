da6b227b4170db30ae919be6136c90d5bc508bd2
GonzaloBarro <gonzalo.barro@cloudx.com>
2022-01-24 10:30:17 -0300

    initial commit

---
+++ apps/www/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="www" />
+      <div />
+    </>
+  );
+}
+
+export default App;
