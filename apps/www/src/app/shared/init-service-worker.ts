e15a44b37b91d8128dfa348d99dd10052a92f5c1
GonzaloBarro <gonzalo.barro@cloudx.com>
2022-02-03 13:59:34 -0300

    fix(shared): remove fixed todo comment for service worker

--- apps/www/src/app/shared/init-service-worker.ts
+++ apps/www/src/app/shared/init-service-worker.ts
@@ -1,13 +1,6 @@
 import { Workbox } from 'workbox-window';
 import { environment } from '../../environments/environment';
 
-/** TODO
- * Fix errors encountered on production serve:
- *  - `Uncaught FirebaseError: Firebase: Error (auth/invalid-api-key).` (<Authenticate>).
- *  - [webpack-dev-server] warnings.
- *  - `Uncaught (in promise) TypeError: ServiceWorker script at http://localhost:4200/sw.js for scope http://localhost:4200/ encountered an error during installation.`
- *  - WARNING in DefinePlugin -> Conflicting values for 'process.env.NODE_ENV' (Terminal server).
- */
 const initServiceWorker = () => {
   if ('serviceWorker' in navigator && environment.production) {
     const wb = new Workbox('/sw.js');
