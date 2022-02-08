75b4c204349b374db985e3da4c6e5eed9b82370a
GonzaloBarro <gonzalo.barro@cloudx.com>
2022-02-08 11:11:41 -0300

    fix(www): mock authorization state for rendering

--- apps/www/src/app/app.spec.tsx
+++ apps/www/src/app/app.spec.tsx
@@ -1,17 +1,31 @@
 import { render } from '@testing-library/react';
-
 import App from './app';
+import initFirebase from './shared/init-firebase';
+
+const mockAuth = jest.fn();
 
 describe('App', () => {
+  beforeAll(() => {
+    jest.mock('react-firebase-hooks/auth', () => {
+      return jest.fn().mockImplementation(() => {
+        return { useAuthState: mockAuth };
+      });
+    });
+
+    mockAuth.mockReturnValue([true, false]);
+
+    initFirebase();
+  });
+
   it('should render successfully', () => {
     const { baseElement } = render(<App />);
 
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
+  it('should have "Sign In" as title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText(/Welcome www/gi)).toBeTruthy();
+    expect(getByText(/Sign In/gi)).toBeTruthy();
   });
 });
