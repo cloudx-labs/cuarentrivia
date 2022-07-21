7f48b9114920c884eb8153fe50c5311ffa148d47
DanielKlugGareis <6662729+DanielKlugGareis@users.noreply.github.com>
2022-07-21 13:40:45 -0300

    Fix(play): fixing friendly name generation (#124)
    * Fix(play): fixing friendly name generation
    
    * Style fix

--- apps/www/src/app/shared/generate-friendly-name.ts
+++ apps/www/src/app/shared/generate-friendly-name.ts
@@ -1,22 +1,9 @@
-const getRandomString = async () => Math.random().toString(16).substring(2, 16);
+import { getFunctions, httpsCallable } from 'firebase/functions';
 
-/** TODO
- * Solved adding async function returning random string.
- * When functions/ folder (under apps/) has been added by the right way,
- * we could use getFunctions and httpCallable to use generateFriendlyName.
- *
- *   import { getFunctions, httpsCallable } from 'firebase/functions';
- *
- *   ...
- *
- *   const functions = getFunctions();
- *   const result = await httpsCallable(functions, 'generateFriendlyName')();
- *   return result.data;
- */
 const generateFriendlyName = async (): Promise<string> => {
-  const data = await getRandomString();
-
-  return data;
+  const functions = getFunctions();
+  const result = await httpsCallable(functions, 'generateFriendlyName')();
+  return typeof result.data === 'string' ? result.data : '';
 };
 
 export default generateFriendlyName;
